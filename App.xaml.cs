70d0f587693bd86774e8b41542d3421772b0fc5c
razeft <alex.gioffre_dev@outlook.it>
2025-04-08 16:14:30 +0200

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace PasswordGenerator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		var window = new Window(new MainPage());
+
+		window.Height = window.MinimumHeight = window.MaximumHeight = 350;
+		window.Width = window.MinimumWidth = window.MaximumWidth = 800;
+
+		return window;
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace PasswordGenerator;
+
+public partial class MainPage : ContentPage
+{
+	String password = "";
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+	private void OnGenerate(object sender, EventArgs e)
+	{
+		password = "";
+		String characters = "abcdefghijklmnopqrstuvzyxABCDEFGHIJKLMONPQRSTUVZXY0123456789!()[]{}?";
+		for (int i = 0; i < 15; i++)
+		{
+			Random rand = new();
+			int index = rand.Next(characters.Length);
+			char value = characters[index];
+			password += value;
+		}
+
+		PasswordText.Text = password;
+		CopyButton.IsEnabled = true;
+	}
+
+	private async void OnCopy(object sender, EventArgs e)
+	{
+		await Clipboard.SetTextAsync(PasswordText.Text);
+
+		PasswordText.Text = "Password copied!";
+		password = "";
+		CopyButton.IsEnabled = false;
+
+		await Task.Delay(1000);
+
+		PasswordText.Text = "Click to generate a new password";
+
+	}
+}
+
